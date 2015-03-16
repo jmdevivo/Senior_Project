21b341a06b19ca6d3e1c037c1136a7d40981b63c
jmdevivo <jdevivo@gmail.com>
2015-03-16 15:13:24 -0400

    Basic Earth Movers Distance Functionality implemented in EarthMover Class

--- EarthMover.scala
+++ EarthMover.scala
@@ -2,8 +2,19 @@ package distance {
 
 	class EarthMover {
 
-		def distance(): String = {
-			return "Distance method correctly called"
+		def distance(arr1: Array[Double], arr2: Array[Double]): Double = {
+			var lastDistance = 0.0
+			var totalDistance = 0.0
+			var currentDistance = 0.0
+			var i = 0
+			while(i < arr1.length){
+				currentDistance = (arr1(i) + lastDistance) - arr2(i)
+				totalDistance += Math.abs(currentDistance)
+				lastDistance = currentDistance
+				i += 1
+			}
+			return currentDistance
+
 		}
 	}
 }
--- Main.scala
+++ Main.scala
@@ -8,7 +8,7 @@ object Main {
 		val set2 = Set(2,3,4,5)
 		println("The Jaccard index is " + jaccard.index(set1, set2))
 		println("The Jaccard distance is "+ jaccard.distance(set1, set2))
-		println(emd.distance())
+		println("The Earth Movers Distance is " + emd.distance(Array(1.0, 1.0, 2.0), Array(0.0, 1.0,2.0)))
 	}
 }
 
