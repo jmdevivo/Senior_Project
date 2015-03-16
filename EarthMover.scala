53203268092757cc1e37893f1663c3929363ac47
jmdevivo <jdevivo@gmail.com>
2015-03-16 00:28:15 -0400

    EarthMover Class created (functionality not implemented yet, just getting a feel for scala package management

---
+++ EarthMover.scala
@@ -0,0 +1,9 @@
+package distance {
+
+	class EarthMover {
+
+		def distance(): String = {
+			return "Distance method correctly called"
+		}
+	}
+}
--- Main.scala
+++ Main.scala
@@ -1,13 +1,14 @@
-import distance.Jaccard
+import distance._
 
 object Main {
 	def main(args: Array[String]) {
 		val jaccard = new Jaccard()
-
+		val emd = new EarthMover()
 		val set1 = Set(1,2,3)
 		val set2 = Set(2,3,4,5)
 		println("The Jaccard index is " + jaccard.index(set1, set2))
 		println("The Jaccard distance is "+ jaccard.distance(set1, set2))
+		println(emd.distance())
 	}
 }
 
